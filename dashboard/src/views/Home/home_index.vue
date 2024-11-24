7de04c546d225f8b590bb9bec1e428bc5994c05c
Rafael Almeida V <104868359+rafaelalmeidaV@users.noreply.github.com>
2024-11-24 18:41:09 -0300

    feat: home page

--- dashboard/src/views/Home/home_index.vue
+++ dashboard/src/views/Home/home_index.vue
@@ -1,3 +1,37 @@
 <template>
-    <h1 class="text-brand-main">aqui eh a home mdss</h1>
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">Feedbacker 2024</p>
+  </div>
 </template>
+
+<script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './custom_header.vue'
+import Contact from './contact_home.vue'
+
+export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {}
+
+    function handleAccountCreate () {}
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
+}
+</script>
