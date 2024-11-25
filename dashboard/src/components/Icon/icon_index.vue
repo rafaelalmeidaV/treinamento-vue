82db533a9dbabe6416f90769ca4c62705cfa236b
Rafael Almeida V <104868359+rafaelalmeidaV@users.noreply.github.com>
2024-11-25 18:30:20 -0300

    feat: icon loading

---
+++ dashboard/src/components/Icon/icon_index.vue
@@ -0,0 +1,13 @@
+<template>
+  <component :is="name" v-bind="$props" />
+</template>
+
+<script>
+import Loading from './loading_index.vue'
+export default {
+  components: { Loading },
+  props: {
+    name: { type: String, required: true }
+  }
+}
+</script>
--- dashboard/src/components/ModalLogin/modal_login_index.vue
+++ dashboard/src/components/ModalLogin/modal_login_index.vue
@@ -29,7 +29,8 @@
       </label>
 
       <button :disabled="state.isLoading" type="submit" :class="{ 'opacity-50': state.isLoading}" class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150">
-        Entrar
+        <icon v-if="state.isLoading" name="loading" class="animate-spin"/>
+        <span v-else>Entrar</span>
       </button>
     </form>
   </div>
@@ -42,8 +43,10 @@ import { useToast } from 'vue-toastification'
 import { useField } from 'vee-validate'
 import { reactive } from 'vue'
 import { validadeEmptyAndLenght3, validadeEmptyAndEmail } from '../../utils/validators.js'
+import Icon from '../Icon/icon_index.vue'
 import services from '../../services/service_index.js'
 export default {
+  components: { Icon },
   setup () {
     const modal = useModal()
     const router = useRouter()
